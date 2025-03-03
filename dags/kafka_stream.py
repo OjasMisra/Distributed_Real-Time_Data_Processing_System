5678dbfddd2f1a3cc2f2c71c998fa9e1edc3fbd8
OjasMisra <misraojas@hotmail.com>
2025-03-02 23:05:48 -0500

    docker setup

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -42,14 +42,14 @@ def stream_data():
     print(json.dumps(res,indent=2))
 
 
-with DAG('user_automation',
-         default_args = default_args,
-         schedule_interval='@daily',
-         catchup=False) as dag:
-
-    streaming_task = PythonOperator(
-        task_id='stream_data_from_api',
-#        python_callable=
-    )
+# with DAG('user_automation',
+#          default_args = default_args,
+#          schedule_interval='@daily',
+#          catchup=False) as dag:
+#
+#     streaming_task = PythonOperator(
+#         task_id='stream_data_from_api',
+# #        python_callable=
+#     )
 
 stream_data()
